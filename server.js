a79bc0e55df5de4a0769d33fa72b6aad1717df3c
TeddyTjoe1990 <her21059@byui.edu>
2023-06-10 16:17:16 +0700

    start from beginning

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./backend/controllers'))
+app.use('/', require('./contact/controllers'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
