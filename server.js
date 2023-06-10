c0e8294f187e1bd2d3d35b3af083a389a00c5f9b
TeddyTjoe1990 <her21059@byui.edu>
2023-06-10 16:18:28 +0700

    edit some data

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./contact/controllers'))
+app.use('/', require('./controllers'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
