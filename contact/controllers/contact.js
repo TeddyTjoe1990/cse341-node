eaa741c50d7ed35e69b99822aa8d792147a0b061
TeddyTjoe1990 <her21059@byui.edu>
2023-06-18 02:14:01 +0700

    edited contact file

--- contact/controllers/contact.js
+++ contact/controllers/contact.js
@@ -40,25 +40,21 @@ const createData = async (req, res) => {
       favoriteColor: req.body.favoriteColor,
       birthday: req.body.birthday
     };
-
     const response = await mongodb.getDb().db('CSE341').collection('Contacts').insertOne(contact);
     if (response.acknowledged) {
-      console.log('Created successfully');
-      res.setHeader('Content-Type', 'application/json');
       res.status(201).json(response);
     } else {
-      res.status(500).json(response.error || 'An error occurred! Contact not created!');
+      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
     }
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err });
+    res.status(500).json(err);
   }
 };
 
-// update contact by id in the database
 const updateData = async (req, res) => {
   try {
     const userId = new ObjectId(req.params.id);
+    // be aware of updateOne if you only want to update specific fields
     const contact = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -70,18 +66,15 @@ const updateData = async (req, res) => {
       .getDb()
       .db('CSE341')
       .collection('Contacts')
-      .updateOne({ _id: userId }, { $set: contact });
+      .replaceOne({ _id: userId }, contact);
     console.log(response);
-    if (response.modifiedCount === 1) {
-      console.log('Updated successfully');
-      res.setHeader('Content-Type', 'application/json');
-      res.status(204).json(response).send;
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
     } else {
-      res.status(500).json(response.error || 'An error occurred!');
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
     }
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err });
+    res.status(500).json(err);
   }
 };
 
